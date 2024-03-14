cc62fffa02415e036bf1f76360e03434fb91f753
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-03-14 13:50:25 -0700

    added array of freelancers

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const freelancers = [
-	{name: Alice, price: 30, occupation: `Writer`},
-	{name: Bob, price: 50, occupation: `Teacher`},
-	{name: Carol, price: 70, occupation: `Programmer`},
-]
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+
+const nameList = document.querySelector(`#namesLi`);
+const nameLiArr =  [...nameList];
+console.log(nameList);
