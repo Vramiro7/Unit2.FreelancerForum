acc9f13ddc9412589bf69f6616c460ee48774ef5
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-04-07 18:00:10 -0700

    created interval and add freelancer functions, rendered freelancer list

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const freelancer = [];
+
+const prices = {
+
+}
+
 const people = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
   { name: "Dr. Pressure", price: 51, occupation: "programmer" },
@@ -6,70 +12,61 @@ const people = [
   { name: "Dr. Impulse", price: 43, occupation: "teacher" },
   { name: "Prof. Spark", price: 76, occupation: "programmer" },
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" }
 ];
 
-const prices = []
-
 const averageDisplay = document.querySelector(`#averagePrice`);
-const freelancerList = document.querySelector(`#infoContainer`); // selected the info container section on html
-const maxPeople = 20; // sets max people to 8
-
-
-
-
-// add freelancers to new array for display 
-const addFreelancer = () => {
-  const randomFreelancer = people[Math.floor(Math.random() * people.length)]; // randomizes a freelancer from our state
-  people.push(randomFreelancer) // pushes into freelancers array 
-  if (people.length === maxPeople){ // if max people are reached
-    clearInterval(addFreelancerIntervalId) // clear interval 
-  }
-};
-
-addFreelancerIntervalId = setInterval(addFreelancer, 5000); // set interval for 5 seconds
-addFreelancer(); 
-
-
-
-  
-  //push person.price to new prices array
-  //loop through new array and add all numbers together
-  // divide the sum of the array by total number of elements in the array
-  // return the quotient 
-;
+const freelancerList = document.querySelector(`#infoContainer`);
+const maxPeople = 8;
 
 const render = () => {
-    people.map((person) => {  // map through people 
-      li = document.createElement(`li`);// create li element  
-      newLi = li.innerText =`Name : ${person.name} --- Price:  ${person.price} --- Occupation : ${person.occupation}`;// set innertext to name, price and occupation  
-      freelancerList.replaceChildren(newLi); //ul.replaceChilren with li elements 
-      // console.log(newLi)
-  }); 
-
+  freelancerList.innerHTML = "";
+  freelancer.forEach((person) => {
+    const li = document.createElement(`li`);
+    const info = `Name: ${person.name} --- Price: ${person.price} --- Occupation: ${person.occupation}`
+    li.innerText = info;
+    freelancerList.appendChild(li);
+    
+  });
 };
 
-render();
+const addFreelancer = () => {
+  if (freelancer.length === maxPeople) {
+    clearInterval(addFreelancerIntervalId); // Clear the interval when max people reached
+  } else {
+    const randomFreelancer = people[Math.floor(Math.random() * people.length)];
+    freelancer.push(randomFreelancer);
+  }
+  render(); // Render the updated list of freelancers
+};
+
+const addFreelancerIntervalId = setInterval(addFreelancer, 5000); // set interval for 5 seconds
+
 
 const getAverage = () => {
-  people.map((lancer) => {
-    prices.push(lancer.price);
+  freelancer.forEach((lancer) => {
+    prices.push(lancer.price); //push lancer.price to new prices array
   });
 
   let sum = 0 
-  for (let i = 0; i < prices.length; i++){
-    sum += prices[i];
+  for (let i = 0; i < prices.length; i++){//loop through new array 
+    sum += prices[i];// add all numbers together
   }
-  return sum/prices.length
+  const average = sum/prices.length
+  return average // divide the sum of the array by total number of elements in the array and return quotient 
 };
 
 getAverage();
 
-const renderGetAverage = () => {
-  p = document.createElement(`p`)
-  p.innerText = getAverage
-  averageDisplay.appendChild(p);
+const renderGetAverage = () => { // create render average function
+  p = document.createElement(`p`) // create new p element 
+  p.innerText = average // set inner text to return of getAverage function 
+  averageDisplay.appendChild(p); // add p to our section for display 
+
 };
 
 renderGetAverage();
 
+  
+  
+  
