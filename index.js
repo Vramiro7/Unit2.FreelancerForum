4a90c70a905c56d553955a420b014e2780aa7ef9
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-04-08 06:57:58 -0700

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const freelancer = [];
 
-const prices = {
 
-}
+const prices = [];
+
 
 const people = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
@@ -12,14 +12,18 @@ const people = [
   { name: "Dr. Impulse", price: 43, occupation: "teacher" },
   { name: "Prof. Spark", price: 76, occupation: "programmer" },
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" }
+  { name: "Andy Button", price: 72, occupation: "project manager" },
+  { name: "Tiburcio James", price: 97, occupation: "audio engineer" },
+  { name: "Zoe Escorza", price: 54, occupation: "musician" },
+  { name: "Alex Smith", price: 44, occupation: "driver" },
+  { name: "Devin Hinch", price: 68, occupation: "mechanic" },
 ];
 
 const averageDisplay = document.querySelector(`#averagePrice`);
 const freelancerList = document.querySelector(`#infoContainer`);
 const maxPeople = 8;
 
-const render = () => {
+const renderFreelancers = () => {
   freelancerList.innerHTML = "";
   freelancer.forEach((person) => {
     const li = document.createElement(`li`);
@@ -37,35 +41,55 @@ const addFreelancer = () => {
     const randomFreelancer = people[Math.floor(Math.random() * people.length)];
     freelancer.push(randomFreelancer);
   }
-  render(); // Render the updated list of freelancers
+  renderFreelancers(); // Render the updated list of freelancers
 };
+
 
 const addFreelancerIntervalId = setInterval(addFreelancer, 5000); // set interval for 5 seconds
 
+const updatePrices = () => {
+  freelancer.map((person) => {
+    prices.push(person.price);
+  })
+  console.log(prices)
+}
 
 const getAverage = () => {
-  freelancer.forEach((lancer) => {
-    prices.push(lancer.price); //push lancer.price to new prices array
-  });
-
-  let sum = 0 
-  for (let i = 0; i < prices.length; i++){//loop through new array 
-    sum += prices[i];// add all numbers together
+  let sum = 0;
+  for (let i = 0; i < freelancer.length; i++){
+    sum += freelancer[i].price;
   }
-  const average = sum/prices.length
-  return average // divide the sum of the array by total number of elements in the array and return quotient 
-};
+  const avg = sum / freelancer.length;
+  return avg 
+}
 
 getAverage();
 
-const renderGetAverage = () => { // create render average function
-  p = document.createElement(`p`) // create new p element 
-  p.innerText = average // set inner text to return of getAverage function 
-  averageDisplay.appendChild(p); // add p to our section for display 
 
-};
 
-renderGetAverage();
+// ORIGINAL CODE, WAS ATTEMPTING TO REWRITE IN ORDER TO SEE WHERE I WENT WRONG
+
+// const getAverage = () => {
+//   freelancer.map((lancer) => {
+//     prices.push(lancer.price); //push lancer.price to new prices array
+//   });
+  
+//   let sum = 0 
+//   for (let i = 0; i < prices.length; i++){//loop through new array 
+//     sum += prices[i];// add all numbers together
+//   }
+//   const priceAverage = sum/prices.length
+//   return priceAverage// divide the sum of the array by total number of elements in the array and return quotient 
+// };
+
+// getAverage();
+
+
+// const renderGetAverage = () => { // create render average function
+  
+// };
+
+// renderGetAverage();
 
   
   
